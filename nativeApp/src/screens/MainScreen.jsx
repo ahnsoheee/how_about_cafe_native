318f161f5210ea1b0fddd2bcd125a993c6c189f4
ahnsoheee <soheeeahn@gmail.com>
2021-11-19 01:47:10 +0900

    [fe] feat: user_id props 추가

--- nativeApp/src/screens/MainScreen.jsx
+++ nativeApp/src/screens/MainScreen.jsx
@@ -9,6 +9,7 @@ const Tab = createBottomTabNavigator();
 
 const MainScreen = ({ navigation }) => {
     const [auth, setAuth] = useState(true);
+    const [user_id, setUserId] = useState('test');
 
     return auth ? (
         <Tab.Navigator
@@ -17,7 +18,9 @@ const MainScreen = ({ navigation }) => {
                 tabBarShowLabel: false
             }}>
             <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarIcon: ({ focused }) => <HomeIcon focused={focused} /> }} />
-            <Tab.Screen name="Settings" component={SettingScreen} options={{ tabBarIcon: ({ focused }) => <SettingIcon focused={focused} /> }} />
+            <Tab.Screen name="Settings" options={{ tabBarIcon: ({ focused }) => <SettingIcon focused={focused} /> }} >
+                {() => <SettingScreen user_id={user_id} />}
+            </Tab.Screen>
         </Tab.Navigator>
     ) : (
         <InitScreen navigation={navigation} />
