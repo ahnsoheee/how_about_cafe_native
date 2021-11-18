35fa8e744ce7be9a6f788d1f5e9dac27d8c644c0
ahnsoheee <soheeeahn@gmail.com>
2021-11-19 01:47:04 +0900

    [fe] feat: 셋팅 화면 구현

---
+++ nativeApp/src/components/common/SettingList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+const SettingList = ({ title, onPress }) => {
+    return (
+        <List onPress={onPress} activeOpacity={1}>
+            <Title>{title}</Title>
+            <Arrow>
+                <Icon name="right" size={16} />
+            </Arrow>
+        </List>
+    )
+}
+
+const List = styled.TouchableOpacity`
+    flex-direction: row;
+    background-color: #ffffff;
+    padding: 15px;
+    padding-left: 20px;
+`;
+
+const Title = styled.Text`
+    font-size: 15px;
+    width: 95%;
+    font-weight: bold;
+`;
+
+const Arrow = styled.View`
+    right: 0;    
+    padding-top: 2px;
+`;
+
+
+export default SettingList
---
+++ nativeApp/src/screens/SettingScreen.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components/native'
+import UserIcon from '../components/common/Icon/UserIcon';
+import { Text } from 'react-native'
+import SettingList from '../components/common/SettingList';
+
+const SettingScreen = ({ user_id }) => {
+    const manageMyReview = () => {
+        // 내가 작성한 리뷰 로직 
+    }
+
+    const registerCafe = () => {
+        // 카페 등록 로직
+    }
+
+    const logout = () => {
+        // 로그아웃 로직
+    }
+
+    return (
+        <>
+            <Header>
+                <UserIcon />
+                <UserId>&nbsp;&nbsp;{user_id}</UserId>
+            </Header>
+            <Text>{"\n"}</Text>
+            <SettingList title="내가 작성한 리뷰" onPress={manageMyReview} />
+            <SettingList title="카페 등록하기" onPress={registerCafe} />
+            <SettingList title="로그아웃" onPress={logout} />
+        </>
+    )
+}
+
+const Header = styled.View`
+    flex-direction: row;
+    padding: 30px;
+    background-color: #FF8E26;
+`;
+
+const UserId = styled.Text`
+    color: #ffffff;
+    font-size: 30px;
+    padding-top: 3px;
+`;
+export default SettingScreen;
