dbaf1ce86682b935d6907f1cb0e7f18bbd724ff9
ahnsoheee <soheeeahn@gmail.com>
2021-11-22 22:21:57 +0900

    [fe] feat: 별점 컴포넌트 구현

--- nativeApp/src/components/common/Star.jsx
+++ nativeApp/src/components/common/Star.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import Icon from 'react-native-vector-icons/EvilIcons';
+import Icon from 'react-native-vector-icons/Entypo';
 
 const Star = ({ star }) => {
     return (
-        <>
-            <Icon name="star" color="red" size={30} />
+        <Wrapper>
+            <Icon name="star" color="orange" size={20} />
             <Value>&nbsp;{star}</Value>
-        </>
+        </Wrapper>
     )
 }
 
+const Wrapper = styled.View`
+    flex-direction: row;
+`;
 
 const Value = styled.Text`
     font-size: 17px;
