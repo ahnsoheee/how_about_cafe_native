ebb6614bcaea0231dca02d015d7a05eaab551d55
ahnsoheee <soheeeahn@gmail.com>
2021-11-22 21:48:30 +0900

    [fe] feat: 리뷰 사진 컴포넌트 구현

---
+++ nativeApp/src/components/common/MorePhoto.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const MorePhoto = ({ uri, cnt }) => {
+    return (
+        <StyldImage source={{ uri: uri }} resizeMode="cover" imageStyle={{ opacity: 0.5 }}>
+            <Number>{"\n+"}{cnt}</Number>
+        </StyldImage>
+    )
+}
+
+const StyldImage = styled.ImageBackground`
+    width: 90px;
+    height: 90px;
+`;
+
+const Number = styled.Text`
+    color: #ffffff;
+    font-weight: bold;
+    font-size: 24px;
+    text-align: center;
+    align-items: center;
+`;
+
+export default MorePhoto;
