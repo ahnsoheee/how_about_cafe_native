62e029ec9300dff76ce0a35b66f1168dbfd583af
ahnsoheee <soheeeahn@gmail.com>
2021-11-27 19:12:48 +0900

    [be] env: 백엔드 환경셋팅

---
+++ howaboutcafe/src/main/java/com/springboot/howaboutcafe/HowaboutcafeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.howaboutcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowaboutcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowaboutcafeApplication.class, args);
+	}
+
+}
---
+++ howaboutcafe/src/test/java/com/springboot/howaboutcafe/HowaboutcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.howaboutcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowaboutcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
