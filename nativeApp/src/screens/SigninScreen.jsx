8b066a0791c63c192e97a9151453db6e561d2572
ahnsoheee <soheeeahn@gmail.com>
2021-12-11 23:01:28 +0900

    [fe] 로그인 기능 구현

--- nativeApp/src/screens/SigninScreen.jsx
+++ nativeApp/src/screens/SigninScreen.jsx
@@ -3,9 +3,12 @@ import Button from '../components/common/Button';
 import Input from '../components/sign/Input';
 import Wrapper from '../components/sign/Wrapper';
 import { Text } from 'react-native';
-import { ToastAndroid, Platform, AlertIOS, } from 'react-native';
+import { ToastAndroid, Platform } from 'react-native';
+import Toast from 'react-native-simple-toast';
+import { API } from "../api/api";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const SigninScreen = () => {
+const SigninScreen = ({ navigation }) => {
     const [id, setId] = useState('');
     const [pw, setPw] = useState('');
 
@@ -17,20 +20,42 @@ const SigninScreen = () => {
         setPw(text);
     };
 
-    const onPress = () => {
+    const storeData = async (key, value) => {
+        await AsyncStorage.setItem(key, value);
+    };
+
+    const onPress = async () => {
         if (!id.length || !pw.length) {
             if (Platform.OS === 'android') {
                 ToastAndroid.show('아이디와 비밀번호를 모두 입력하세요', ToastAndroid.SHORT, ToastAndroid.CENTER);
             } else {
-                AlertIOS.alert('아이디와 비밀번호를 모두 입력하세요');
+                Toast.show('아이디와 비밀번호를 모두 입력하세요', Toast.SHORT);
             }
         } else {
 
-            // 로그인 로직 추가
+            const res = await API.post('/signin', {
+                'user_id': id,
+                'pw': pw
+            });
+
+            if (res.status) {
+                try {
+                    storeData('token', res.result);
+                    navigation.navigate('Main');
+                } catch (e) {
+                    // 에러 처리
+                }
+            } else {
+                if (Platform.OS === 'android') {
+                    ToastAndroid.show(res.result, ToastAndroid.SHORT, ToastAndroid.CENTER);
+                } else {
+                    Toast.show(res.result, Toast.SHORT);
+                }
+            }
         }
     };
-    return (
 
+    return (
         <Wrapper>
             <Input type="text" placeholder="아이디" value={id} onChange={(text) => onChangeId(text)} />
             <Text>{"\n"}</Text>
