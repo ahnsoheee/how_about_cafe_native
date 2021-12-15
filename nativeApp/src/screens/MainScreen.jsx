0de00174192f67ddfdefd76398bab3040ecb4e2b
ahnsoheee <soheeeahn@gmail.com>
2021-12-15 22:38:03 +0900

    [fe] feat: 인증 구현

--- nativeApp/src/screens/MainScreen.jsx
+++ nativeApp/src/screens/MainScreen.jsx
@@ -1,20 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import InitScreen from './InitScreen';
 import HomeScreen from './HomeScreen';
 import SettingScreen from './SettingScreen';
 import HomeIcon from '../components/icon/HomeIcon';
 import SettingIcon from '../components/icon/SettingIcon';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { API } from '../api/api';
 
 const Tab = createBottomTabNavigator();
 
 const MainScreen = ({ navigation }) => {
     const [auth, setAuth] = useState(false);
-    const [user_id, setUserId] = useState('');
+    const [user_name, setUserName] = useState('');
     const [home, setHome] = useState(true);
     const [setting, setSetting] = useState(false);
     const [search, setSearch] = useState(false);
     const [searchValue, setSearchValue] = useState('');
+
+    useEffect(() => {
+
+        const getAuth = async (token) => {
+            const res = await API.post("/auth", token);
+            setUserName(res.result);
+            setAuth(res.status);
+        };
+
+        AsyncStorage.getItem("token").then((token) => {
+            if (token) {
+                getAuth(token);
+            }
+        });
+    }, [auth, user_name]);
 
     return auth ? (
         <Tab.Navigator
@@ -54,7 +71,7 @@ const MainScreen = ({ navigation }) => {
                 }}
             >
                 {() => <SettingScreen
-                    user_id={user_id}
+                    user_name={user_name}
                     navigation={navigation} />}
             </Tab.Screen>
         </Tab.Navigator>
