a7911ba9753baa22c3f4ceb59d5eeccadc028e32
ahnsoheee <soheeeahn@gmail.com>
2021-12-15 23:20:55 +0900

    [be] feat: signup 메소드 네임 수정

--- howaboutcafe/src/main/java/com/springboot/howaboutcafe/controller/UserController.java
+++ howaboutcafe/src/main/java/com/springboot/howaboutcafe/controller/UserController.java
@@ -2,14 +2,10 @@ package com.springboot.howaboutcafe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
 
 import com.springboot.howaboutcafe.dto.ResponseDTO;
 import com.springboot.howaboutcafe.dto.UserDTO;
