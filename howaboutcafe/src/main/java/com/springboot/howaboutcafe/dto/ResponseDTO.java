79b6cdbb91cb87addf3b4f6ef5ee43934d6e87b6
ahnsoheee <soheeeahn@gmail.com>
2021-12-17 00:12:00 +0900

    [be] feat: lombok annotation 수정

--- howaboutcafe/src/main/java/com/springboot/howaboutcafe/dto/ResponseDTO.java
+++ howaboutcafe/src/main/java/com/springboot/howaboutcafe/dto/ResponseDTO.java
@@ -2,9 +2,11 @@ package com.springboot.howaboutcafe.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ResponseDTO {
     boolean status;
     String result;
--- howaboutcafe/src/main/java/com/springboot/howaboutcafe/dto/UserDTO.java
+++ howaboutcafe/src/main/java/com/springboot/howaboutcafe/dto/UserDTO.java
@@ -1,12 +1,15 @@
 package com.springboot.howaboutcafe.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 public class UserDTO {
 
     String user_id;
     String pw;
     String user_name;
-    
 }
