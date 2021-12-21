dce0ef07d4a03b283b44fd4d4b51885b3eaf70c8
ahnsoheee <soheeeahn@gmail.com>
2021-12-21 23:27:35 +0900

    #13 [fe] refactor: 처음 mount 될때만 API 호출

--- nativeApp/src/screens/MyReviewScreen.jsx
+++ nativeApp/src/screens/MyReviewScreen.jsx
@@ -31,7 +31,7 @@ const MyReviewScreen = ({ navigation, route }) => {
                 if (result) setReviews(result);
             };
             getMyReviews();
-        })
+        }, [])
     );
 
     return (
