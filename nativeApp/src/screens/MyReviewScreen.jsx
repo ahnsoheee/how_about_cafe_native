e2c64a1bad7fc92b9e5daf1656d863cd2c5b693d
ahnsoheee <soheeeahn@gmail.com>
2021-12-26 01:36:37 +0900

    #13 [fe] feat: FlatList 적용

--- nativeApp/src/screens/MyReviewScreen.jsx
+++ nativeApp/src/screens/MyReviewScreen.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState, useCallback } from 'react';
 import styled from 'styled-components/native';
 import { API } from '../api/api';
-import MyReviewList from '../components/common/MyReviewList';
 import { View, Text } from 'react-native';
 import { Modal, Provider } from '@ant-design/react-native';
 import SimpleToast from "react-native-simple-toast";
-import { useEffect } from "react";
+import StarRating from "react-native-star-rating";
 
 const MyReviewScreen = ({ navigation, route }) => {
     const user_id = route.params.user_id;
@@ -21,8 +20,17 @@ const MyReviewScreen = ({ navigation, route }) => {
 
     useEffect(() => {
         getMyReviews();
-    }, [pressedId]);
+        return () => {
+            setVisible(false);
+            setPressedId(review_id);
+        };
+    }, []);
 
+
+    const onPress = (review_id) => {
+        setPressedId(review_id);
+        setVisible(true);
+    };
 
     const getMyReviews = useCallback(async () => {
         const result = await API.get(`/review?user_id=${user_id}`);
@@ -32,12 +40,43 @@ const MyReviewScreen = ({ navigation, route }) => {
     const deleteReview = async () => {
         const res = await API.patch(`/review/${pressedId}/delete`);
         SimpleToast.show(res.result, SimpleToast.SHORT);
-        setPressedId(0);
+        getMyReviews();
     };
+
+    const myReviewList = ({ item }) => (
+        <Review key={item.review_id}>
+            <TopWrapper>
+                <LeftWrapper>
+                    <CafeName>{item.cafe_name}</CafeName>
+                    <MiddleWrapper>
+                        <StarWrapper>
+                            <StarRating
+                                disabled={true}
+                                maxStars={5}
+                                starSize={13}
+                                fullStarColor={"#FF8E26"}
+                                emptyStarColor={"#FF8E26"}
+                                rating={item.star} />
+                        </StarWrapper>
+                        <Date>{item.created_at.substring(0, 10)}</Date>
+                    </MiddleWrapper>
+                </LeftWrapper>
+                <DeleteButton onPress={() => onPress(item.review_id)}>삭제</DeleteButton>
+            </TopWrapper>
+            <Content>{item.content}</Content>
+            {item.image ? <Photo source={{ uri: item.image }} /> : <></>}
+        </Review>
+    );
 
     return (
         <Wrapper>
-            <MyReviewList reviews={reviews} setVisible={setVisible} setPressedId={setPressedId} />
+            <List
+                data={reviews}
+                renderItem={myReviewList}
+                numColumns={1}
+                keyExtractor={(review) => review.review_id}
+            />
+
             <Provider>
                 <Modal
                     transparent={true}
@@ -56,6 +95,68 @@ const MyReviewScreen = ({ navigation, route }) => {
         </Wrapper>
     );
 };
+
+
+const Review = styled.View`
+    width: 100%;
+    height: auto;
+    flex-direction: column;
+    background-color: #ffffff;
+    border: 1px solid #dddddd;
+    border-radius: 10px;
+    overflow: visible;
+    padding: 13px;  
+    margin-bottom: 20px;
+    box-shadow: 5px 5px 5px #dddddd;
+    elevation: 3;
+`;
+
+const TopWrapper = styled.View`
+    width: 100%;
+    flex-direction: row;
+`;
+
+const LeftWrapper = styled.View`
+    flex-grow: 9;
+`;
+
+const MiddleWrapper = styled.View`
+    flex-direction: row;
+    margin-bottom: 5px;
+`;
+
+const DeleteButton = styled.Text`
+`;
+
+const CafeName = styled.Text`
+    font-weight: bold;
+    font-size: 18px;
+    margin-bottom: 5px;
+`;
+
+const StarWrapper = styled.View`
+    margin-bottom: 3px;
+    margin-right: 3px;
+`;
+
+const Date = styled.Text`
+    font-size: 12px;
+    color: #848484;
+`;
+
+const Content = styled.Text`
+    font-size: 19px;
+    margin-bottom: 10px;
+`;
+
+const Photo = styled.Image`
+    width: 100%;
+    height: 300px;
+`;
+
+const List = styled.FlatList`
+    margin: 2px;  
+`;
 
 const Wrapper = styled.View`
     width: 100%;
