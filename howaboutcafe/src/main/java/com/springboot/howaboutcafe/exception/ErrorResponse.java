fa9b780b498a6d86773f21308703fd0152611553
ahnsoheee <soheeeahn@gmail.com>
2022-01-19 23:56:10 +0900

    [be] feat: ErrorResponse class 작성

---
+++ howaboutcafe/src/main/java/com/springboot/howaboutcafe/exception/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.springboot.howaboutcafe.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private String code;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.status = errorCode.getStatus();
+        this.code = errorCode.getErrorCode();
+    }
+}
