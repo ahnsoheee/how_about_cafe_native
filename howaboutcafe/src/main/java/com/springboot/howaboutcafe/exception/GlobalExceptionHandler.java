7578c8a012dda472b034b63c7c706a7a7c53311b
ahnsoheee <soheeeahn@gmail.com>
2022-01-20 20:53:55 +0900

    #19 [be] refactor: Exception 오류 수정

--- howaboutcafe/src/main/java/com/springboot/howaboutcafe/exception/GlobalExceptionHandler.java
+++ howaboutcafe/src/main/java/com/springboot/howaboutcafe/exception/GlobalExceptionHandler.java
@@ -10,43 +10,44 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateException.class)
     public ResponseEntity<ErrorResponse> handleDuplicateException(DuplicateException e) {
-        return new ResponseEntity<ErrorResponse>(new ErrorResponse(e.getErrorCode()),
+        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<ErrorResponse> handleForbiddenException(ForbiddenException e) {
-        return new ResponseEntity<ErrorResponse>(new ErrorResponse(e.getErrorCode()),
+        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(InternalServerException.class)
-    public ResponseEntity<ErrorResponse> handleInternalServerException(ForbiddenException e) {
-        return new ResponseEntity<ErrorResponse>(new ErrorResponse(e.getErrorCode()),
+    public ResponseEntity<ErrorResponse> handleInternalServerException(InternalServerException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(InvalidException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidException(ForbiddenException e) {
-        return new ResponseEntity<ErrorResponse>(new ErrorResponse(e.getErrorCode()),
+    public ResponseEntity<ErrorResponse> handleInvalidException(InvalidException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleNotFoundException(ForbiddenException e) {
-        return new ResponseEntity<ErrorResponse>(new ErrorResponse(e.getErrorCode()),
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<ErrorResponse> handleUnauthorizedException(ForbiddenException e) {
-        return new ResponseEntity<ErrorResponse>(new ErrorResponse(e.getErrorCode()),
+    public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException e) {
+        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception ex) {
-        return new ResponseEntity<>(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR),
+        return new ResponseEntity<>(
+                new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, "Internal Server Error"),
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
