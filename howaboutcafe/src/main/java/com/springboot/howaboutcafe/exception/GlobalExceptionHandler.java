6b5863087acd480e1271d967ac5eb68be45d7f01
ahnsoheee <soheeeahn@gmail.com>
2022-01-21 16:39:32 +0900

    #19 [be] refactor: Sentry 적용

--- howaboutcafe/src/main/java/com/springboot/howaboutcafe/exception/GlobalExceptionHandler.java
+++ howaboutcafe/src/main/java/com/springboot/howaboutcafe/exception/GlobalExceptionHandler.java
@@ -5,47 +5,56 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import io.sentry.Sentry;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicateException.class)
     public ResponseEntity<ErrorResponse> handleDuplicateException(DuplicateException e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<ErrorResponse> handleForbiddenException(ForbiddenException e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(InternalServerException.class)
     public ResponseEntity<ErrorResponse> handleInternalServerException(InternalServerException e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(InvalidException.class)
     public ResponseEntity<ErrorResponse> handleInvalidException(InvalidException e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> handleUnauthorizedException(UnauthorizedException e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(new ErrorResponse(e.getErrorCode(), e.getMessage()),
                 HttpStatus.valueOf(e.getErrorCode().getStatus()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        Sentry.captureException(e);
         return new ResponseEntity<>(
                 new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, "Internal Server Error"),
                 HttpStatus.INTERNAL_SERVER_ERROR);
